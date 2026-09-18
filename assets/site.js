c0e3981e03e548a4aed09391cc003244036871f9
AlekseyMajeski <46598080+AlekseyMajeski@users.noreply.github.com>
2026-09-17 23:14:07 -0300

    Load BIXOLON catalog in site search

--- assets/site.js
+++ assets/site.js
@@ -13,6 +13,7 @@
     source.replace('impressoras.json','impressoras-brother.json'),
     source.replace('impressoras.json','impressoras-honeywell.json'),
     source.replace('impressoras.json','impressoras-sato.json'),
+    source.replace('impressoras.json','impressoras-bixolon.json'),
     source.replace('impressoras.json','impressoras-tsc.json'),
     source.replace('impressoras.json','impressoras-tsc-healthcare.json'),
     source.replace('impressoras.json','impressoras-tsc-rfid.json'),
